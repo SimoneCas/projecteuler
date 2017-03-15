462ef2a5ba0fb4a01ca58724909e4a076aa8324e
Casamassa Simone <s.casamassa@reply.it>
2017-03-15 18:19:37 +0100

    commit 1 - condivisione progetto 1 per project euler

---
+++ projectEuler/src/main/java/it/simonecasamassa/projectEuler/probl1/Solution.java
@@ -0,0 +1,23 @@
+package it.simonecasamassa.projectEuler.probl1;
+
+public class Solution {
+
+	private final int START = 0;
+	private final int END = 1000;
+
+	public static void main(String[] args) {
+		new Solution().printSolution();
+	}
+
+	public void printSolution() {
+		int somma = 0;
+
+		for (int i = 0; i < this.END; i++) {
+			if (i % 3 == 0 || i % 5 == 0)
+				somma += i;
+		}
+
+		System.out.println("La somma dei multipli di 3 e 5 tra " + START + " e " + END + " = " + somma);
+
+	}
+}
